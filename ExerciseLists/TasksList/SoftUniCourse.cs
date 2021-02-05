136efe58cdf773ab58627e0ca4891b463fd67aff
Miroslav Stoynov <m.pen4ev88@gmail.com>
2021-02-05 16:45:38 +0200

    Update ExerciseList Task 10 (SoftUniCourse)

--- ExerciseLists/TasksList/SoftUniCourse.cs
+++ ExerciseLists/TasksList/SoftUniCourse.cs
@@ -9,111 +9,142 @@ namespace ExerciseLists.TasksList
     {
         public void TaskSoftUniCourse()
         {
-            List<string> baseSchedule = Console.ReadLine().Split(", ").ToList();
+            string breakProgram = "course start";
+            string addCommand = "Add:";
+            string removeCommand = "Remove:";
+            string insertCommand = "Insert:";
+            string swapCommand = "Swap:";
+            string exerciseCommand = "Exercise:";
+            string exerciseToLesson = "Exercise";
+
+            Dictionary<string, bool> dictLessonExercise = new Dictionary<string, bool>();
+
+            List<string> baseScheduleList = Console.ReadLine().Split(", ").ToList();
             string tmpString = "";
 
-            while(true)
+            while((tmpString = Console.ReadLine()) != breakProgram )
             {   
-                tmpString = Console.ReadLine();
-
-                if(tmpString == "course start")
+                if(tmpString.Contains(addCommand))
                 {
-                    break;
-                }                
+                    //string newCourse = tmpString.Skip(4).ToString();
+                    string newCourse =  tmpString.Remove(0, 4);
 
-                if(tmpString.Contains("Add:"))
-                {
-                    string newCourse= tmpString.Remove(0, 4);
-
-                    if (!baseSchedule.Contains(newCourse))
+                    if (!baseScheduleList.Contains(newCourse))
                     {
-                        baseSchedule.Add(newCourse);
+                        baseScheduleList.Add(newCourse);
+                        dictLessonExercise.Add(newCourse, false);
                     }
                 }
-                else if(tmpString.Contains("Remove:"))
+                else if(tmpString.Contains(removeCommand))
                 {
-                    string newCourse= tmpString.Remove(0, 7);
+                    //string delCourse = tmpString.Skip(7).ToString();
+                    string delCourse =  tmpString.Remove(0, 7);
 
-                    if(baseSchedule.Contains(newCourse))
+                    if(baseScheduleList.Contains(delCourse))
                     {
-                        baseSchedule.Remove(newCourse);
-                        if(baseSchedule.Contains(newCourse +"-Exercise"))
+                        baseScheduleList.Remove(delCourse);
+                        dictLessonExercise.Remove(delCourse);
+                        if (baseScheduleList.Contains(delCourse + "-Exercise"))
                         {
-                            baseSchedule.Remove(newCourse +"-Exercise");
+                            baseScheduleList.Remove(delCourse + "-Exercise");
                         }
                     }
                 }
-                else if(tmpString.Contains("Insert:"))
+                else if(tmpString.Contains(insertCommand))
                 {
-                    string newCourse= tmpString.Remove(0, 7);
-                    string[] insString = newCourse.Split(":");
+                    //string newCourse = tmpString.Skip(7).ToString();
+                    string newCourse =  tmpString.Remove(0, 7);
 
-                    if (!baseSchedule.Contains(insString[0]))
-                    {
-                        int position = int.Parse(insString[1]);
-                        baseSchedule.Insert(position, insString[0]);
+                    string[] insString = newCourse.Split(":");
+                    int position = int.Parse(insString[1]);
+                    string lesson = insString[0];
+
+                    if (!baseScheduleList.Contains(lesson))
+                    {                        
+                        baseScheduleList.Insert(position, lesson);
+                        dictLessonExercise.Add(lesson, false);
                     }
                 }
-                 else if(tmpString.Contains("Swap:"))
+                 else if(tmpString.Contains(swapCommand))
                 {
                     string newCourse= tmpString.Remove(0, 5);
                     string[] swapString = newCourse.Split(":");
+                    string swapFirst = swapString[0];
+                    string swapNext = swapString[1];
 
-                    if (baseSchedule.Contains(swapString[0]) && baseSchedule.Contains(swapString[1]))
+                    if (baseScheduleList.Contains(swapFirst) && baseScheduleList.Contains(swapNext))
                     {
-                        int indxToSwapFirst = baseSchedule.IndexOf(swapString[0]);                        
-                        int indxToSwapSecond = baseSchedule.IndexOf(swapString[1]);
+                        int indxToSwapFirst = baseScheduleList.IndexOf(swapFirst);                        
+                        int indxToSwapSecond = baseScheduleList.IndexOf(swapNext);
 
-                        baseSchedule.Insert(indxToSwapFirst, swapString[1].ToString());
+                        baseScheduleList.Insert(indxToSwapFirst, swapNext.ToString());                        
+                        baseScheduleList.Insert(indxToSwapSecond + 1, swapFirst.ToString());
+                        baseScheduleList.RemoveAt(indxToSwapFirst + 1);
+                        baseScheduleList.RemoveAt(indxToSwapSecond + 1);
 
-                        if (baseSchedule.Contains(swapString[1] + "-Exercise"))
-                        {
-                            int indxExercise = baseSchedule.IndexOf(swapString[1] + "-Exercise");
-                            baseSchedule.Insert(indxToSwapFirst + 1, (swapString[1] + "-Exercise").ToString());
-                            baseSchedule.RemoveAt(indxExercise + 1);
-                            baseSchedule.RemoveAt(indxToSwapFirst + 2);
-                            indxToSwapSecond++;
-                        }
-                        else
-                        {
-                            baseSchedule.RemoveAt(indxToSwapFirst + 1);
-                        }
+                        //baseScheduleList.Insert(indxToSwapFirst, swapNext.ToString());
 
-                        baseSchedule.Insert(indxToSwapSecond, swapString[0].ToString());
+                        //if (baseScheduleList.Contains(swapNext + "-Exercise"))
+                        //{
+                        //    int indxExercise = baseScheduleList.IndexOf(swapNext + "-Exercise");
+                        //    baseScheduleList.Insert(indxToSwapFirst + 1, (swapNext + "-Exercise").ToString());
+                        //    baseScheduleList.RemoveAt(indxExercise + 1);
+                        //    baseScheduleList.RemoveAt(indxToSwapFirst + 2);
+                        //    indxToSwapSecond++;
+                        //}
+                        //else
+                        //{
+                        //    baseScheduleList.RemoveAt(indxToSwapFirst + 1);
+                        //}
 
-                        if (baseSchedule.Contains(swapString[0] + "-Exercise"))
-                        {
-                            int indxExercise = baseSchedule.IndexOf(swapString[0] + "-Exercise");
-                            baseSchedule.Insert(indxToSwapSecond + 1, (swapString[0] + "-Exercise").ToString());
-                            baseSchedule.RemoveAt(indxExercise + 1);
-                            baseSchedule.RemoveAt(indxToSwapSecond + 2);
-                        }
-                        else
-                        {
-                            baseSchedule.RemoveAt(indxToSwapSecond + 1);
-                        }
+                        //baseScheduleList.Insert(indxToSwapSecond, swapFirst.ToString());
+
+                        //if (baseScheduleList.Contains(swapFirst + "-Exercise"))
+                        //{
+                        //    int indxExercise = baseScheduleList.IndexOf(swapFirst + "-Exercise");
+                        //    baseScheduleList.Insert(indxToSwapSecond + 1, (swapFirst + "-Exercise").ToString());
+                        //    baseScheduleList.RemoveAt(indxExercise + 1);
+                        //    baseScheduleList.RemoveAt(indxToSwapSecond + 2);
+                        //}
+                        //else
+                        //{
+                        //    baseScheduleList.RemoveAt(indxToSwapSecond + 1);
+                        //}
                     }
                 }          
-                 else if(tmpString.Contains("Exercise:"))
+                 else if(tmpString.Contains(exerciseCommand))
                 { 
+                    //string lessonExercise = tmpString.Skip(9).ToString();
                     string lessonExercise = tmpString.Remove(0, 9);
 
-                    if (!baseSchedule.Contains(lessonExercise))
-                    {
-                        baseSchedule.Add(lessonExercise);
-                        baseSchedule.Add(lessonExercise + "-Exercise");                        
+                    if (!baseScheduleList.Contains(lessonExercise))
+                    {                        
+                        baseScheduleList.Add(lessonExercise);
+                        dictLessonExercise.Add(lessonExercise, true);
+                        //baseScheduleList.Add(lessonExercise + "-Exercise");                        
                     }
                     else
                     {
-                        int indx = baseSchedule.IndexOf(lessonExercise);
-                        baseSchedule.Insert(indx , lessonExercise + "-Exercise");
+                        //int indx = baseScheduleList.IndexOf(lessonExercise);
+                        dictLessonExercise[lessonExercise] = true;
+                        //baseScheduleList.Insert(indx , lessonExercise + "-Exercise");
                     }
                 }                      
             }
 
-            for (int i = 0; i < baseSchedule.Count; i++)
+            int outputCount = 1;
+
+            for (int i = 0; i < baseScheduleList.Count; i++)
             {
-                Console.WriteLine($"{i + 1}.{baseSchedule[i]}");
+                Console.WriteLine($"{outputCount}.{baseScheduleList[i]}");
+                if(dictLessonExercise.ContainsKey(baseScheduleList[i]))
+                {
+                    if(dictLessonExercise[baseScheduleList[i]])
+                    {
+                        Console.WriteLine($"{outputCount + 1}.{baseScheduleList[i]}-{exerciseToLesson}");
+                    }
+                }
+                outputCount++;
             }
         }
     }
